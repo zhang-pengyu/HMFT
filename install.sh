215cb846fc5da59e4e7810ac92bdc085840b0270
Pengyu Zhang <67819706+zhang-pengyu@users.noreply.github.com>
2023-03-25 20:40:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,12 +38,4 @@ gdown https://drive.google.com/uc?id=1vnof9qMFsfwmn8xk-UKaFhHTYM1F85j2 -O OptimT
 
 echo ""
 echo ""
-echo "****************** Download demo sequences ******************"
-gdown https://drive.google.com/uc?id=1LeYyYxPA5XBF4m73T1p9qXsmGKVV7Fmm -O mfDiMP/demo/bus1.zip
-gdown https://drive.google.com/uc?id=1yrtTYKcUfNsk-jxjXmY54FIxtBS3Fz9H -O mfDiMP/demo/bike_man.zip
-gdown https://drive.google.com/uc?id=1uiqFuG3RnWcyu-OaRzKIQhrCLXKKM7_z -O mfDiMP/demo/car.zip
-gdown https://drive.google.com/uc?id=1fL4Yo4xBSndeE_ShAqrraiHAL7QvrEAi -O mfDiMP/demo/night_car.zip
-
-echo ""
-echo ""
 echo "****************** Installation complete! ******************"
